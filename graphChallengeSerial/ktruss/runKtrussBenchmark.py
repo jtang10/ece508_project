852bf8f058837485a01f7e295bf4636a144fc79d
jtang10 <tangjingning@gmail.com>
2019-04-09 13:45:01 -0500

    added graph challenge serial code. fixed ktruss. Not sure if triangle works

---
+++ graphChallengeSerial/ktruss/runKtrussBenchmark.py
@@ -0,0 +1,33 @@
+import numpy as np
+import scipy as sp
+from ktruss import ktruss
+import os, sys, argparse
+
+#Use the pandas package if available
+#import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_filename", nargs="?", action="store", type=str, default="../data/ktruss_example.tsv")
+args = parser.parse_args()
+
+inc_mtx_file = args.input_filename
+
+if not os.path.isfile(inc_mtx_file):
+	print("File doesn't exist: '{}'!".format(inc_mtx_file))
+	sys.exit(1)
+
+E = ktruss(inc_mtx_file,3)
+print("ktruss result: \n{}".format(E))
+
+
+###################################################
+# Graph Challenge benchmark
+# Developer: Dr. Vijay Gadepally (vijayg@mit.edu)
+# MIT
+
+# Modified by Jingning. Fixed issues for Python3 
+# compatibility and np dtype issue
+###################################################
+
+
+
